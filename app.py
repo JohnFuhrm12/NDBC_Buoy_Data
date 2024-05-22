d7914450fdee96af7a887ff08fe4f2d60a1690bb
John <johnfuhrmeister12@gmail.com>
2024-05-22 13:00:22 -0400

    Return valid JSON file for buoy data

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 @app.route("/buoy/<buoy_id>")
 def getBuoyData(buoy_id):
-    BuoyInfo = read_csv(f"https://www.ndbc.noaa.gov/data/realtime2/{buoy_id}.txt")
-    print(BuoyInfo)
-    return {"id": buoy_id}
+    buoy_csv = f"https://www.ndbc.noaa.gov/data/realtime2/{buoy_id}.txt"
+    buoy_data_df = read_csv(buoy_csv, delim_whitespace=True)
+    json_output = buoy_data_df.to_json(indent=1, orient="records")
+    return json_output
 
 if __name__ == "main":
     app.run(debug=True)
--- noaa.py
+++ noaa.py
@@ -1,5 +1,12 @@
 import pandas as pd
+ 
 
-BuoyInfo = pd.read_csv('https://www.ndbc.noaa.gov/data/realtime2/51201.txt', delim_whitespace=True)
+buoy_csv = "https://www.ndbc.noaa.gov/data/realtime2/51201.txt"
+buoy_data_df = pd.read_csv(buoy_csv, sep="\s+")
 
-print(BuoyInfo)
+json_path = "./51201.json"
+json_output = buoy_data_df.to_json(json_path, indent=1, orient="records")
+
+selected_json = buoy_data_df.to_json(orient='records', lines=True)
+
+#print(selected_json)
