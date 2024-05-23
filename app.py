d12fe8e6ec716d3ce1e7f37dc78166f49558ac41
John <johnfuhrmeister12@gmail.com>
2024-05-22 21:39:19 -0400

    Enabled cors and allowed cross origins

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask
 from pandas import read_csv
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/')
+@cross_origin()
+def hello_world():
+    return 'Hello from Flask!'
+
+@app.route('/test')
+@cross_origin()
+def test():
+    return 'Test Successful!'
 
 @app.route("/buoy/<buoy_id>")
+@cross_origin()
 def getBuoyData(buoy_id):
     buoy_csv = f"https://www.ndbc.noaa.gov/data/realtime2/{buoy_id}.txt"
     buoy_data_df = read_csv(buoy_csv, delim_whitespace=True)
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import requests
 
-BASE = "http://127.0.0.1:5000/"
+BASE = "https://johnfuhrm12.pythonanywhere.com/"
+#BASE = "http://127.0.0.1:5000/"
 
-response = requests.get(BASE + "buoy/51201")
+response = requests.get(BASE  + "buoy/51201")
 print(response.json())
